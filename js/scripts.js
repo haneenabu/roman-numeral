6adde8c9cc01bcd0d9b1f5455a738e9268834bd7
Esti Shay and Haneen AbuKhater <haneen@>
2017-07-13 12:03:41 -0700

    refactoring functions for multiple use

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,17 @@ function numToConvert(userInput){
   } else if ((userInput < 0) || (userInput > 3999)){
     alert("Please enter a number between 1-3999")
   } else if (userInput.length === 4){
-    divides = thousands(userInput);
+    var test = parseInt(userInput.length) -4;
+    divides = thousands(userInput, test);
+
+    var test1 = parseInt(userInput.length) -3;
+    divides += hundreds(userInput, test1);
+
+    var test2 = parseInt(userInput.length) -2;
+    divides += tens(userInput, test2);
+    var test3 = parseInt(userInput.length) -1;
+    divides += ones(userInput, test3);
+    alert (divides);
     return divides;
   } else if (userInput %5 <= 3){
     return userInput;
@@ -17,54 +27,67 @@ function numToConvert(userInput){
   }
 }
 
-function thousands(userInput){
+function thousands(userInput, test){
   var roman = '';
   //for 1st place
-  for (var i = userInput[0]; i >0 ; i--) {
+  for (var i = userInput[test]; i >0 ; i--) {
     roman += 'M';
   }
+  return roman;
+}
+function hundreds(userInput, test1){
   //second place
-  if (userInput[1] <= 3) {
-    for (var i = userInput[1]; i >0 ; i--) {
+  var roman = '';
+  alert(test1);
+  if (userInput[test1] <= 3) {
+    for (var i = userInput[test1]; i >0 ; i--) {
       roman += 'C';
     }
-  }else if (userInput[1] === '4') {
+  }else if (userInput[test1] === '4') {
     roman += 'CD';
-  }else if (userInput[1] >= 5 && userInput[1] !== '9') {
+  }else if (userInput[test1] >= 5 && userInput[test1] !== '9') {
     roman += 'D';
-    var num = parseInt(userInput[1]) - 5;
+    var num = parseInt(userInput[test1]) - 5;
     for (var i = num; i >0; i--) {
       roman += 'C';
     }
   }else {
     roman += 'CM';
   }
+  return roman;
+}
+function tens(userInput, test2){
+  var roman = '';
   //third place
-  if (userInput[2] <= 3) {
-    for (var i = userInput[2]; i >0 ; i--) {
+  if (userInput[test2] <= 3) {
+    for (var i = userInput[test2]; i >0 ; i--) {
       roman += 'X';
     }
-  }else if (userInput[2] === '4') {
+  }else if (userInput[test2] === '4') {
     roman += 'XL';
-  }else if (userInput[2] >= 5 && userInput[2] !== '9') {
+  }else if (userInput[test2] >= 5 && userInput[test2] !== '9') {
     roman += 'L';
-    var num = parseInt(userInput[2]) - 5;
+    var num = parseInt(userInput[test2]) - 5;
     for (var i = num; i >0; i--) {
       roman += 'X';
     }
   }else {
     roman += 'LC';
   }
+  return roman;
+}
+function ones(userInput, test3){
+  var roman = '';
   //fourth place
-  if (userInput[3] <= 3) {
-    for (var i = userInput[3]; i >0 ; i--) {
+  if (userInput[test3] <= 3) {
+    for (var i = userInput[test3]; i >0 ; i--) {
       roman += 'I';
     }
-  }else if (userInput[3] === '4') {
+  }else if (userInput[test3] === '4') {
     roman += 'IV';
-  }else if (userInput[3] >= 5 && userInput[3] !== '9') {
+  }else if (userInput[test3] >= 5 && userInput[test3] !== '9') {
     roman += 'V';
-    var num = parseInt(userInput[3]) - 5;
+    var num = parseInt(userInput[test3]) - 5;
     for (var i = num; i >0; i--) {
       roman += 'I';
     }
