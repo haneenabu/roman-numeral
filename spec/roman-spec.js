4f9f055d3019eb7718b997ae61c7ec19cf534c26
Haneen AbuKhater <abukhater.haneen@gmail.com>
2017-09-06 11:21:42 -0700

    add 12 jasmine tests

--- spec/roman-spec.js
+++ spec/roman-spec.js
@@ -3,29 +3,58 @@ var homies = require('./../js/scripts.js').pickles;
 
 describe('roman-numeral', function() {
 
-
-
   it('should test result for 1 as input', function(){
+    var output = RomanConverter("1")
+    expect(output).toEqual('I')
+  });
+  it('should test result for 5 as input', function(){
+    var output = RomanConverter("5")
+    expect(output).toEqual('V')
+  });
+  it('should test result for 10 as input', function(){
+    var output = RomanConverter("10")
+    expect(output).toEqual('X')
+  });
+  it('should test result for 50 as input', function(){
+    var output = RomanConverter("50")
+    expect(output).toEqual('L')
+  });
+  it('should test result for 100 as input', function(){
+    var output = RomanConverter("100")
+    expect(output).toEqual('C')
+  });
+  it('should test result for 500 as input', function(){
+    var output = RomanConverter("500")
+    expect(output).toEqual('D')
+  });
+  it('should test result for 1000 as input', function(){
+    var output = RomanConverter("1000")
+    expect(output).toEqual('M')
+  });
+  it('should test result for 4 as input', function(){
     var output = RomanConverter("4")
-    console.log(output)
     expect(output).toEqual('IV')
   });
-
-  it('should test result for 1 as input', function(){
-    var output = new homies(4)
-    // console.log(output.sayHi())
-    expect(output.sayHi()).toEqual('hello')
+  it('should test result for 40 as input', function(){
+    var output = RomanConverter("40")
+    expect(output).toEqual('XL')
   });
-
-
-
-
-  // it('should test result for 1 as input', function(){
-  //   var romanConverter = = new RomanConverter(4);
-  //
-  //   var output = romanConverter.NumToConvert();
-  //
-  //   console.log(output)
-  //   expect(output).toEqual('IV')
+  it('should test result for 400 as input', function(){
+    var output = RomanConverter("400")
+    expect(output).toEqual('CD')
+  });
+  it('should test result for 9 as input', function(){
+    var output = RomanConverter("9")
+    expect(output).toEqual('IX')
+  });
+  it('should test result for 90 as input', function(){
+    var output = RomanConverter("90")
+    expect(output).toEqual('XC')
+  });
+  // it('should test result for 5 as input', function(){
+  //   var output = new homies(4)
+  //   // console.log(output.sayHi())
+  //   expect(output.sayHi()).toEqual('hello')
   // });
+
 });
